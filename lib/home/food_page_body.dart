7e30c566bfc6e777a2ff31701f886917053f65a7
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-04-01 23:10:15 +0700

    feat: change food_page_body and add icon_and_text

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,5 +1,13 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:my_app/utils/color.dart';
+import 'package:my_app/widgets/big_text.dart';
+import 'package:my_app/widgets/icon_and_text_widget.dart';
+import 'package:my_app/widgets/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({Key? key}) : super(key: key);
@@ -9,10 +17,27 @@ class FoodPageBody extends StatefulWidget {
 
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
+  var _currPageValue = 0.0;
+  double _scaleFactor = 0.8;
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color.fromARGB(255, 222, 213, 107),
+      //color: Color.fromARGB(255, 222, 213, 107),
       height: 320,
       child: PageView.builder(
           controller: pageController,
@@ -24,30 +49,108 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Stack(
-      children: [
-        Container(
-          height: 220,
-          margin: EdgeInsets.only(left: 5, right: 5),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
-              image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage("assets/image/food0.png"))),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 140,
-            margin: EdgeInsets.only(left: 40, right: 40, bottom: 15),
+    Matrix4 matrix = Matrix4.identity();
+    double pageIndex = _currPageValue.floor().toDouble();
+    double nextPageIndex = pageIndex + 1;
+
+    if (index == pageIndex) {
+      double currScale = 1 - (_currPageValue - pageIndex) * (1 - _scaleFactor);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+    } else if (index == nextPageIndex) {
+      double currScale =
+          _scaleFactor + (pageIndex + 1 - _currPageValue) * (1 - _scaleFactor);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+    }
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
+            margin: EdgeInsets.only(left: 10, right: 10),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/image/food0.png"))),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 120,
+              margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                        color: Color(0xFFe8e8e8),
+                        blurRadius: 5.0,
+                        offset: Offset(0, 5)),
+                    BoxShadow(color: Colors.white, offset: Offset(-5, 0)),
+                    BoxShadow(color: Colors.white, offset: Offset(5, 0))
+                  ]),
+              child: Container(
+                padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Salat Rau Củ Quả"),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(5, (index) {
+                            return Icon(
+                              Icons.star,
+                              color: AppColor.mainColor,
+                              size: 15,
+                            );
+                          }),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "4.5"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "1287"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "bình luận")
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        IconAndTextWidget(
+                            icon: Icons.circle_sharp,
+                            text: "Bình thường",
+                            iconColor: AppColor.iconColor1),
+                        IconAndTextWidget(
+                            icon: Icons.location_on,
+                            text: "1.7km",
+                            iconColor: AppColor.mainColor),
+                        IconAndTextWidget(
+                            icon: Icons.access_alarm_rounded,
+                            text: "32phút",
+                            iconColor: AppColor.iconColor2),
+                      ],
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
